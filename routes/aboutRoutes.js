5103a700c09aecce2fa5e7a4cd137e9a1d44e57b
SushantLakhera <94135836+hybrid000@users.noreply.github.com>
2023-07-22 20:36:30 +0530

    Add files via upload

--- routes/aboutRoutes.js
+++ routes/aboutRoutes.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const aboutContent =
-    'Content for the contact page goes here';
+const aboutContent = 'Content for the contact page goes here';
+
 router.get('/', (req, res) => {
-    res.render('about', { inputabout: aboutContent });
+    try {
+        res.render('about', { inputabout: aboutContent });
+    } catch (err) {
+        // Handle the error gracefully
+        console.error('Error rendering the about page:', err);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 module.exports = router;
--- routes/composeRoutes.js
+++ routes/composeRoutes.js
@@ -7,15 +7,21 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-    const post = new composedPosts({
-        title: req.body.postTitle,
-        author: req.body.authorName,
-        content: req.body.postBody,
-        originalContent: req.body.postBody, // Fix typo here: req.postBody -> req.body.postBody
-    });
+    try {
+        const post = new composedPosts({
+            title: req.body.postTitle,
+            author: req.body.authorName,
+            content: req.body.postBody,
+            originalContent: req.body.postBody, // Fix typo here: req.postBody -> req.body.postBody
+        });
 
-    await post.save();
-    res.redirect('/');
+        await post.save();
+        res.redirect('/');
+    } catch (err) {
+        // Handle the error gracefully
+        console.error('Error saving the post:', err);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 module.exports = router;
