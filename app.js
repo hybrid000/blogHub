c8947ba9a93f068df8d05a7e40f86e0410cf2ab5
Sushant <sushnatlakhera2912@gmail.com>
2023-07-22 20:39:56 +0530

    latest 2.0

--- app.js
+++ app.js
@@ -12,23 +12,37 @@ const aboutRoutes = require('./routes/aboutRoutes');
 const composeRoutes = require('./routes/composeRoutes');
 const postRoutes = require('./routes/postRoutes');
 
-
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-const PORT=process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
+
 });
 
-const connectDB = require("./dbconnection");
-connectDB();
+// Add the MongoDB connection code here
+const mongooseConnectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DB_connection || "mongodb+srv://sushantlakhera2912:01NThero2912@bloghub.n30fnlr.mongodb.net/?retryWrites=true&w=majority", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("DB connected");
+  } catch (error) {
+    console.error("Error connecting to the database:", error.message);
+    process.exit(1); // Exit the application with a non-zero status code to indicate an error
+  }
+};
+
+mongooseConnectDB();
+
 // Import the Mongoose schema file
 const composedPosts = require('./models/composeModel');
-
 
 // Use the route files
 app.use('/', homeRoutes);
@@ -36,4 +50,3 @@ app.use('/edit', editRoutes);
 app.use('/about', aboutRoutes);
 app.use('/compose', composeRoutes);
 app.use('/posts', postRoutes);
-
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -3,56 +3,87 @@ const router = express.Router();
 const composedPosts = require('../models/composeModel');
 
 router.get('/:postId', async (req, res) => {
-    const requestedPostId = req.params.postId;
-    const post = await composedPosts.findOne({ _id: requestedPostId });
-    res.render('post', {
-        title: post.title,
-        content: post.content,
-        postId: post._id,
-        author: post.author,
-    });
+    try {
+        const requestedPostId = req.params.postId;
+        const post = await composedPosts.findOne({ _id: requestedPostId });
+        if (!post) {
+            // Handle the case when the requested post is not found
+            return res.status(404).send('Post not found');
+        }
+        res.render('post', {
+            title: post.title,
+            content: post.content,
+            postId: post._id,
+            author: post.author,
+        });
+    } catch (err) {
+        // Handle the error gracefully
+        console.error('Error retrieving the post:', err);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 router.post('/delete/:postId', async (req, res) => {
-    const deletePostId = req.params.postId;
+    try {
+        const deletePostId = req.params.postId;
 
-    await composedPosts.findOneAndDelete({ _id: deletePostId });
-    res.send(
-        '<script>alert("Post deleted successfully"); window.location.href = "/";</script>'
-    );
+        await composedPosts.findOneAndDelete({ _id: deletePostId });
+        res.send(
+            '<script>alert("Post deleted successfully"); window.location.href = "/";</script>'
+        );
+    } catch (err) {
+        // Handle the error gracefully
+        console.error('Error deleting the post:', err);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 router.get('/edit/:postId', async (req, res) => {
-    const editPostId = req.params.postId;
-    const post = await composedPosts.findOne({ _id: editPostId });
-    res.render('edit', {
-        title: post.title,
-        content: post.content,
-        postId: post._id,
-        author: post.author,
-    });
+    try {
+        const editPostId = req.params.postId;
+        const post = await composedPosts.findOne({ _id: editPostId });
+        if (!post) {
+            // Handle the case when the requested post is not found
+            return res.status(404).send('Post not found');
+        }
+        res.render('edit', {
+            title: post.title,
+            content: post.content,
+            postId: post._id,
+            author: post.author,
+        });
+    } catch (err) {
+        // Handle the error gracefully
+        console.error('Error retrieving the post for editing:', err);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 router.post('/edit/:postId', async (req, res) => {
-    const saveButtonClicked = req.body.saveButton !== undefined;
-    const discardButtonClicked = req.body.discardButton !== undefined;
-    const requestedPostId = req.params.postId;
+    try {
+        const saveButtonClicked = req.body.saveButton !== undefined;
+        const discardButtonClicked = req.body.discardButton !== undefined;
+        const requestedPostId = req.params.postId;
 
-    if (saveButtonClicked) {
-        const updatedTitle = req.body.postTitle;
-        const updatedAuthor = req.body.authorName;
-        const updatedContent = req.body.postBody;
+        if (saveButtonClicked) {
+            const updatedTitle = req.body.postTitle;
+            const updatedAuthor = req.body.authorName;
+            const updatedContent = req.body.postBody;
 
-        await composedPosts.findByIdAndUpdate(requestedPostId, {
-            $set: {
-                title: updatedTitle,
-                content: updatedContent,
-                author: updatedAuthor,
-            },
-        });
+            await composedPosts.findByIdAndUpdate(requestedPostId, {
+                $set: {
+                    title: updatedTitle,
+                    content: updatedContent,
+                    author: updatedAuthor,
+                },
+            });
+        }
+
         res.redirect('/posts/' + requestedPostId);
-    } else {
-        res.redirect('/posts/' + requestedPostId);
+    } catch (err) {
+        // Handle the error gracefully
+        console.error('Error saving the edited post:', err);
+        res.status(500).send('Internal Server Error');
     }
 });
 
